22dc857aa6eace33bc5ab1ff29f7e3c60654eea4
dgoel1 <dishigoel93@gmail.com>
2018-12-04 01:55:55 -0500

    Update: .gitignore LICENSE README.md cleaning notebooks

--- notebooks/Users/disha.goel@rhsmith.umd.edu/Mortality_bigdata.py
+++ notebooks/Users/disha.goel@rhsmith.umd.edu/Mortality_bigdata.py
@@ -91,15 +91,49 @@ from pyspark.sql.functions import col, countDistinct
 
 # COMMAND ----------
 
-df_11.groupBy('entity_condition_15').count().orderBy('count', ascending=False).show()
+df_11.groupBy('record_condition_5').count().orderBy('count', ascending=False).show()
 
 # COMMAND ----------
 
-df_12 = df_11.drop('record_condition_20' , 'entity_condition_20', 'entity_condition_19', 'entity_condition_18', 'entity_condition_17', 'record_condition_19', 'record_condition_18', 'record_condition_16', 'record_condition_17','record_condition_15','record_condition_14','record_condition_13','record_condition_12','record_condition_11','record_condition_10','record_condition_9','record_condition_8','record_condition_7' , 'record_condition_6', 'entity_condition_16', 'entity_condition_15','entity_condition_14', 'entity_condition_13', 'entity_condition_12', 'entity_condition_11', 'entity_condition_10', 'entity_condition_9','entity_condition_8', 'entity_condition_7', 'entity_condition_6')
+df_12 = df_11.drop('record_condition_20' , 'entity_condition_20', 'entity_condition_19', 'entity_condition_18', 'entity_condition_17', 'record_condition_19', 'record_condition_18', 'record_condition_16', 'record_condition_17','record_condition_15','record_condition_14','record_condition_13','record_condition_12','record_condition_11','record_condition_10','record_condition_9','record_condition_8','record_condition_7' , 'record_condition_6', 'record_condition_5', 'entity_condition_16', 'entity_condition_15','entity_condition_14', 'entity_condition_13', 'entity_condition_12', 'entity_condition_11', 'entity_condition_10', 'entity_condition_9','entity_condition_8', 'entity_condition_7', 'entity_condition_6', 'entity_condition_5')
 
 # COMMAND ----------
 
 df_12.groupBy('current_data_year').count().orderBy('count', ascending=False).show()
+
+# COMMAND ----------
+
+df_12 = df_12.fillna({'place_of_injury_for_causes_w00_y34_except_y06_and_y07_' : 10})
+df_12.groupBy('place_of_injury_for_causes_w00_y34_except_y06_and_y07_').count().orderBy('count', ascending=False).show()
+
+# COMMAND ----------
+
+df_12 = df_12.fillna({'130_infant_cause_recode' : 000})
+df_12.groupBy('130_infant_cause_recode').count().orderBy('count', ascending=False).show()
+
+# COMMAND ----------
+
+df_12 = df_12.fillna({'activity_code' : 10})
+df_12.groupBy('activity_code').count().orderBy('count', ascending=False).show()
+
+# COMMAND ----------
+
+from pyspark.sql.functions import *
+df_12 = df_12.withColumn('method_of_disposition', regexp_replace('method_of_disposition', 'R' , 'O'))
+df_12 = df_12.withColumn('method_of_disposition', regexp_replace('method_of_disposition', 'E' , 'O'))
+df_12 = df_12.withColumn('method_of_disposition', regexp_replace('method_of_disposition', 'D' , 'O'))
+df_12.groupBy('method_of_disposition').count().orderBy('count', ascending=False).show()
+
+# COMMAND ----------
+
+df_12 = df_12.fillna({'manner_of_death' : 8})
+df_12.groupBy('manner_of_death').count().orderBy('count', ascending=False).show()
+
+# COMMAND ----------
+
+from pyspark.sql.functions import when 
+df_12 = df_12.withColumn('Place_of_death_and_decedents_status', when(df_12['Place_of_death_and_decedents_status']== 9 , 7).otherwise(df['Place_of_death_and_decedents_status']))
+df_12.groupBy('Place_of_death_and_decedents_status').count().orderBy('count', ascending=False).show()
 
 # COMMAND ----------
 
